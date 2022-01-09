dc07c658d91788e20cec0be74c8c4a80fe64be33
hgssnk <60138403+hgssnk@users.noreply.github.com>
2022-01-10 01:32:37 +0900

    Create input.py

---
+++ input.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import ENavi
+
+# 定数
+URL = "https://hoge"
+USER_ID = "9999"
+USER_PASSWORD = "9999"
+DATE = "01/09"
+START_TIME = "08:00"
+END_TIME = "18:30"
+WORK_STATUS = "0"
+COMMENT = "在宅勤務"
+LOG_FILE_PATH = "./"
+
+def main():
+    try:
+        # ENaviクラスのインスタンス化
+        enavi = ENavi.ENavi()
+        print("インスタンス化\t：OK")
+        # ログイン画面
+        enavi.enavi_login(URL, USER_ID, USER_PASSWORD)
+        print("ログイン\t：OK")
+        # トップ画面
+        enavi.enavi_top()
+        print("トップ画面\t：OK")
+        # 月次勤怠画面
+        enavi.enavi_month(DATE)
+        print("月次勤怠画面\t：OK")
+        # 勤怠入力画面
+        enavi.enavi_input(WORK_STATUS, START_TIME, END_TIME, COMMENT)
+        print("勤怠入力画面\t：OK")
+        # 承認依頼画面
+        enavi.enavi_confirm(LOG_FILE_PATH)
+        print("承認依頼画面\t：OK")
+        print("TOTAL\t：正常終了")
+    # 例外処理
+    except Exception as e:
+        print("こけた")
+        print(e)
+    # 終了処理
+    finally:
+        enavi.enavi_end()
+
+if __name__ == "__main__":
+    main()
